f4518d4efc45b8555cfd788228e5624249ffb50f
cdk <lcfcdk@163.com>
2025-03-10 14:46:41 +0800

    Initial commit

---
+++ src/main/java/com/windforce/m2/m2ideaplugin/action/SvnUpdateDesignTrunkDirectoryAction.java
@@ -0,0 +1,23 @@
+package com.windforce.m2.m2ideaplugin.action;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.MessageType;
+
+public class SvnUpdateDesignTrunkDirectory extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        NotificationGroup notificationGroup = new NotificationGroup("testid", NotificationDisplayType.BALLOON, false);
+        /**
+         * content :  通知内容
+         * type  ：通知的类型，warning,info,error
+         */
+        Notification notification = notificationGroup.createNotification("测试通知", MessageType.INFO);
+        Notifications.Bus.notify(notification);
+    }
+}
